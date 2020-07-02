7dc14875675232c81f6fe5e6b18ab970631c66ac
Vladhus <49909976+Vladhus@users.noreply.github.com>
2020-07-02 03:44:46 +0200

    update server.js add new tables

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ var app = require('express')(); // Create an instance of an Express app
 var mobileApp = require('azure-mobile-apps')(); // Create an instance of a Mobile App with default settings
 
 mobileApp.tables.add('UserLogin'); // Create a table for 'Book' with default settings
+mobileApp.tables.add('Delivery'); // Create a table for 'Book' with default settings
+mobileApp.tables.add('DeliveryPerson'); // Create a table for 'Book' with default settings
 
 app.use(mobileApp);
 app.listen(process.env.PORT || 3000);
